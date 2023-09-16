89edf6f86e1c7527bf9653ed75139ee6875dc7af
Yevhenii Kashtalian <djin.kashtalyan@gmail.com>
2023-09-16 22:46:13 +0300

    menu fix

--- Book-Library/src/Menu.java
+++ Book-Library/src/Menu.java
@@ -10,13 +10,13 @@ public class Menu {
     public void displayMenu() {
         boolean exitRequset = false;
 
-        System.out.println("WELCOME TO THE LIBRARY!\n\n" + "PLEASE, SELECT ONE OF THE FOLLOWING " +
-                "ACTIONS BY TYPING THE OPTION'S NUMBER AND PRESSING ENTER KEY:\n" +
-                "\t[1]SHOW ALL BOOKS IN THE LIBRARY\n" +
-                "\t[2]SHOW ALL READERS REGISTERED IN THE LIBRARY\n\n" +
-                "TYPE \"EXIT\" TO STOP THE PROGRAM AND EXIT! ");
 
         while (!exitRequset){
+            System.out.println("WELCOME TO THE LIBRARY!\n\n" + "PLEASE, SELECT ONE OF THE FOLLOWING " +
+                    "ACTIONS BY TYPING THE OPTION'S NUMBER AND PRESSING ENTER KEY:\n" +
+                    "\t[1]SHOW ALL BOOKS IN THE LIBRARY\n" +
+                    "\t[2]SHOW ALL READERS REGISTERED IN THE LIBRARY\n\n" +
+                    "TYPE \"EXIT\" TO STOP THE PROGRAM AND EXIT! ");
 
             Scanner scanner = new Scanner(System.in);
             String option = scanner.nextLine();
