0325f78898a865e9a3cd23739a0d718f53f6b8ee
Yevhenii Kashtalian <djin.kashtalyan@gmail.com>
2023-09-19 00:49:13 +0300

    fix fields

--- Book-Library/src/Menu.java
+++ Book-Library/src/Menu.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Menu {
-  private final Library library = new Library();
-  private final Scanner scanner = new Scanner(System.in);
   private static final String WELCOME_MESSAGE = "WELCOME TO THE LIBRARY!";
   private static final String OPTIONS_MESSAGE =
       """
-                PLEASE, SELECT ONE OF THE FOLLOWING
-                ACTIONS BY TYPING THE OPTION'S NUMBER AND PRESSING ENTER KEY:
-                    [1]SHOW ALL BOOKS IN THE LIBRARY
-                    [2]SHOW ALL READERS REGISTERED IN THE LIBRARY
+                    PLEASE, SELECT ONE OF THE FOLLOWING
+                    ACTIONS BY TYPING THE OPTION'S NUMBER AND PRESSING ENTER KEY:
+                        [1]SHOW ALL BOOKS IN THE LIBRARY
+                        [2]SHOW ALL READERS REGISTERED IN THE LIBRARY
 
-                TYPE "EXIT" TO STOP THE PROGRAM AND EXIT!
-                """;
+                    TYPE "EXIT" TO STOP THE PROGRAM AND EXIT!
+                    """;
   private static final String SEPARATOR =
       "-------------------------------------------------------------------";
+  private final Library library = new Library();
+  private final Scanner scanner = new Scanner(System.in);
 
   public void displayMenu() {
-    boolean displayOptions = true;
+    boolean showOptions = true;
     System.out.println(SEPARATOR);
     System.out.println(WELCOME_MESSAGE + "\n\n" + OPTIONS_MESSAGE);
     boolean exitRequest = false;
@@ -29,11 +29,11 @@ public class Menu {
         case "exit" -> {
           System.out.println("Goodbye!");
           exitRequest = true;
-          displayOptions = false;
+          showOptions = false;
         }
         default -> System.out.println("Invalid option, please write correct option from the menu.");
       }
-      if (displayOptions) {
+      if (showOptions) {
         System.out.println(SEPARATOR);
         System.out.println(OPTIONS_MESSAGE);
       }
