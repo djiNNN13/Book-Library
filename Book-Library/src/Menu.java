d345080a514befc1462f6bd1a17a0013895d4f92
Yevhenii Kashtalian <djin.kashtalyan@gmail.com>
2023-09-19 00:32:19 +0300

    switch statement fix + static fields

--- Book-Library/src/Menu.java
+++ Book-Library/src/Menu.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 
 public class Menu {
-  private final Library library;
-  private final Scanner scanner;
-  private final String welcomeMessage;
-  private final String optionsMessage;
-  private final String separator =
-      "-------------------------------------------------------------------";
-
-  public Menu() {
-    library = new Library();
-    welcomeMessage = "WELCOME TO THE LIBRARY!";
-    optionsMessage =
-        """
+  private final Library library = new Library();
+  private final Scanner scanner = new Scanner(System.in);
+  private static final String WELCOME_MESSAGE = "WELCOME TO THE LIBRARY!";
+  private static final String OPTIONS_MESSAGE =
+      """
                 PLEASE, SELECT ONE OF THE FOLLOWING
                 ACTIONS BY TYPING THE OPTION'S NUMBER AND PRESSING ENTER KEY:
                     [1]SHOW ALL BOOKS IN THE LIBRARY
@@ -20,28 +13,29 @@ public class Menu {
 
                 TYPE "EXIT" TO STOP THE PROGRAM AND EXIT!
                 """;
-    scanner = new Scanner(System.in);
-  }
+  private static final String SEPARATOR =
+      "-------------------------------------------------------------------";
 
   public void displayMenu() {
-    System.out.println(separator);
-    System.out.println(welcomeMessage + "\n\n" + optionsMessage);
+    boolean displayOptions = true;
+    System.out.println(SEPARATOR);
+    System.out.println(WELCOME_MESSAGE + "\n\n" + OPTIONS_MESSAGE);
     boolean exitRequest = false;
     while (!exitRequest) {
       String option = scanner.nextLine().toLowerCase();
-      System.out.println(separator);
-      System.out.println(optionsMessage);
       switch (option) {
         case "1" -> showAllBooks();
         case "2" -> showAllReaders();
         case "exit" -> {
-          exit();
+          System.out.println("Goodbye!");
           exitRequest = true;
+          displayOptions = false;
         }
-        default -> {
-          System.out.println("Invalid option, please write correct option from the menu.");
-          System.out.println(separator);
-        }
+        default -> System.out.println("Invalid option, please write correct option from the menu.");
+      }
+      if (displayOptions) {
+        System.out.println(SEPARATOR);
+        System.out.println(OPTIONS_MESSAGE);
       }
     }
   }
@@ -52,10 +46,5 @@ public class Menu {
 
   private void showAllReaders() {
     library.getReaders().forEach(System.out::println);
-  }
-
-  private void exit() {
-    System.out.println("Goodbye");
-    System.out.println(separator);
   }
 }
